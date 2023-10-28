0e5f2c4353b99e6207e698d0296116d1c5d0e9ce
Arfaz Khan <arfuzkhan@gmail.com>
2023-10-28 19:06:06 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-
 import argparse
 from parser_2 import PdfParser
 
 def main():
-    parser = argparse.ArgumentParser(description='PDF parser endpoint')
-    parser.add_argument('-f', '--file', help='File to parse')
+    parser = argparse.ArgumentParser(description='PDF parsing tool - Extract text and data from PDF files')
+    parser.add_argument('-f', '--file', help='File to parse (e.g., python main.py -f <path to your pdf>)')
     parser.add_argument('-d', '--debug', help='Debug mode', action='store_true')
 
     args = parser.parse_args()
+
+    if not args.file:
+        parser.print_help()
+        return
+
     file_path = args.file
     debug_mode = args.debug
 
-    if file_path:
-        # Initialize PdfParser with the provided file path
-        pdf_parser = PdfParser(file_path)
-        pdf_parser.parse_pdf(file_path, debug_val=debug_mode)
-    else:
-        print("Please provide a file to parse.")
+    # Initialize PdfParser with the provided file path
+    pdf_parser = PdfParser(file_path)
+    pdf_parser.parse_pdf(file_path, debug_val=debug_mode)
 
 if __name__ == "__main__":
     main()
